c7deea6630ab107d8ef699b5dd74aaafb5f3e995
Victor <victorkrook96@gmail.com>
2021-04-23 17:11:20 +0200

    Made a main program for testing during development

---
+++ protocol/src/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+
+#define DEBUG 1
+
+#include "iotoro.h"
+#include "iotoro_linux.h"
+
+const char* deviceId = "Hello world";
+const char* deviceKey = "Hello world key";
+
+
+//IotoroClient con(deviceId, deviceKey);
+ 
+uint8_t sensor1 = 0;
+int8_t sensor2 = 1;
+
+
+
+
+
+
+int main() { 
+    //lincon.test();
+
+} 
+
+
+/*
+    con.setParam("X1", &sensor1);
+    con.setParam("X2", &sensor2);
+    sensor1 = 2;
+    sensor2 = 4;
+*/
